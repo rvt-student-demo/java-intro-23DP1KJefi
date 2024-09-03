66bd379dbc2e479904de2d3df0de703e40689da2
23DP1KJefi <166118535+23DP1KJefi@users.noreply.github.com>
2024-09-03 09:43:48 +0000

    fix config

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,8 +7,16 @@ public class App
 {
     public static void main( String[] args )
     {
-       System.out.println("Hello world!");
+       //System.out.println("Hello world!");
+       //My code starts here
+        String name = "Kamilla";
+        int age = 16;
+        boolean isOnline = false;
 
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isOnline);
+        //Code ends here
     }
 
 }
