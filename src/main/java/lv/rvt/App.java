f1c493e81feaf557876fa3004821297c33a718e4
23DP1KJefi <166118535+23DP1KJefi@users.noreply.github.com>
2024-09-17 09:25:01 +0000

    if number

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,18 +11,16 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
         
-        int cipars1 = Integer.valueOf(scanner.nextLine());
-
-        int cipars2 = Integer.valueOf(scanner.nextLine());
-
-        int cipars3 = Integer.valueOf(scanner.nextLine());
-
-        int summa = cipars1 + cipars2 + cipars3;
-        System.out.println("Summa ir: " + summa);
-        int reizinajums = cipars1 * cipars2 * cipars3;
-        System.out.println("Reizinajums ir: " + reizinajums);
-        double videjais = (double) (cipars1 + cipars2 + cipars3) / 3;
-        System.out.println("Videjais ir: " + videjais);
+        int number = 4;
+        if (number==1){
+            System.out.println("The number is one");
+        } else if (number==2){
+            System.out.println("The number is two");
+        } else if (number==3){
+            System.out.println("The number is three");
+        } else {
+            System.out.println("Something else");
+        }
     }
 
 }
